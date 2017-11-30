64aad67de58381432e826f61772a9decefdf5662
Tarun Kumar Sukhu <tksukhu@gmail.com>
2017-11-30 12:09:18 +0530

    Completed project

---
+++ src/main.js
@@ -0,0 +1,43 @@
+import Vue from 'vue';
+import './style.scss';
+
+import genres from './util/genres';
+
+
+import VueResource from 'vue-resource';
+
+Vue.use(VueResource);
+
+import moment from 'moment-timezone';
+moment.tz.setDefault('UTC');
+Object.defineProperty(Vue.prototype, '$moment', { get() {return this.$root.moment;}});
+
+import { checkFilter } from './util/bus';
+const bus = new Vue();
+Object.defineProperty(Vue.prototype, '$bus', { get() {return this.$root.bus;}});
+
+import VueRouter from 'vue-router';
+Vue.use(VueRouter);
+
+import routes from './util/routes';
+const router = new VueRouter({ routes });
+
+new Vue({
+    el: '#app',
+    data: {
+        genre: [],
+        time: [],
+        movies: [],
+        moment,
+        day: moment(),
+        bus
+    },
+    created() {
+        this.$http.get('/api').then(response => {
+            this.movies = response.data;
+        });
+        // event listener
+        this.$bus.$on('check-filter',checkFilter.bind(this));
+    },
+    router
+});
